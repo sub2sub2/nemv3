045a699190df098cafa0eccd418eeab86a4f3365
sub2sub2 <52900741+sub2sub2@users.noreply.github.com>
2020-01-18 21:23:30 +0900

    start_project

---
+++ fe/src/views/About.vue
@@ -0,0 +1,51 @@
+<template>
+  <v-container grid-list-md text-xs-center>
+    <v-layout row wrap>
+      <v-flex xs12 sm6 md4 lg3 xl2>
+      <v-card color="red">
+        a
+      </v-card>
+    </v-flex>
+    <v-flex xs12 sm6 md4 lg3 xl2>
+      <v-card color="blue">
+        b
+      </v-card>
+    </v-flex>
+    <v-flex xs12 sm6 md4 lg3 xl2>
+      <v-card color="red">
+        c
+      </v-card>
+    </v-flex>
+    <v-flex xs12 sm6 md4 lg3 xl2>
+      <v-card color="blue">
+        d
+      </v-card>
+    </v-flex>
+    <v-flex xs12 sm6 md4 lg3 xl2>
+      <v-card color="red">
+        e
+      </v-card>
+    </v-flex>
+    <v-flex xs12 sm6 md4 lg3 xl2>
+      <v-card color="blue">
+        f
+      </v-card>
+    </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'about',
+  data () {
+    return {
+      ta: 'debug'
+    }
+  },
+  mounted () {
+    this.ta = JSON.stringify(this.$vuetiry.breakpoint)
+  }
+
+}
+</script>
---
+++ fe/src/views/help.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <h1>{{title}}</h1>
+    <button @click="test">test</button>
+    <v-btn :color="btnColor" @click="test">test</v-btn>
+    <v-chip close color="primary">Example Chip</v-chip>
+    <v-chip v-for="chip in chips" :color="chip.cl" v-bind:key="chip">{{chip.name}}</v-chip>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'help',
+  data () {
+    return {
+      title: 'Vuetify.js',
+      btnColor: 'success',
+      chips: [
+        {
+          cl: 'primary',
+          name: 'aaaaa'
+        },
+        {
+          cl: 'secondary',
+          name: 'bbbbb'
+        },
+        {
+          cl: 'green',
+          name: 'ccccc'
+        },
+        {
+          cl: 'warning',
+          name: 'dddd'
+        }
+      ]
+    }
+  },
+  methods: {
+    test () {
+      this.title = 'nnnnnnn'
+      this.btnColor = 'warning'
+    }
+  }
+}
+</script>
